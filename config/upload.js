14a6c043c74c92abab37a26fcbaf43a3785b5c07
rasla yoosafali <raslayoosafali@youzefs-MacBook-Air.local>
2026-02-06 12:55:08 +0530

    updated

--- config/upload.js
+++ config/upload.js
@@ -4,9 +4,7 @@ import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 
-/* ===============================
-   __dirname replacement (ESM)
-================================ */
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
